4fb0240e706e3fd0b7bb7d18827558e8d68ab779
Juchli Bruno Lorenz <bruno.lorenz.juchli@erowa.com>
2019-09-27 18:35:02 +0200

    nesting: doesn't work

--- src/Xbehave.Execution/StepReporter.cs
+++ src/Xbehave.Execution/StepReporter.cs
@@ -20,18 +20,47 @@ namespace Xbehave.Execution
         }
 
 
-        public void Success(string stepName) =>
+        // doesn't show test name - we're just part of the scenario.
+        //public void Success(string stepName) =>
+        //    this.messageBus.Queue(
+        //        this.scenario, test => new TestPassed(test, default, "fabulous"), this.cancellationTokenSource);
+
+        // is being ignored
+        //public void Success(string stepName) =>
+        //    this.messageBus.Queue(
+        //        new Step(this.scenario, "successful step"), test => new TestPassed(test, default, "fabulous"), this.cancellationTokenSource);
+
+
+        public void Success(string stepName)
+        {
+            this.messageBus.QueueMessage(new TestStarting(this.scenario));
+
+            var step = new Step(this.scenario, stepName);
+            this.messageBus.QueueMessage(new TestStarting(step));
+
+            var innerStep = new Step(this.scenario, "innerStep");
+
+            this.messageBus.QueueMessage(new TestStarting(innerStep));
+
+            this.messageBus.QueueMessage(new TestPassed(innerStep, default, "inner test"));
+
+            this.messageBus.QueueMessage(new TestFinished(innerStep, default, "inner test done"));
+
+            this.messageBus.QueueMessage(new TestPassed(step, default, "fabulous"));
+
+            this.messageBus.QueueMessage(new TestFinished(step, default, null));
+
+            this.messageBus.QueueMessage(new TestPassed(this.scenario, default, null));
+
+            this.messageBus.QueueMessage(new TestFinished(this.scenario, default, null));
+        }
+
+        public void Ignored(string stepName) =>
             this.messageBus.Queue(
-                this.scenario, test => new TestPassed(test, default, string.Empty), this.cancellationTokenSource);
+                this.scenario, test => new TestSkipped(test, "don't know'"), this.cancellationTokenSource);
 
-        public void Ignored(string stepName)
-        {
-
-        }
-
-        public void Failure(string stepName, Exception exception)
-        {
-
-        }
+        public void Failure(string stepName, Exception exception) =>
+            this.messageBus.Queue(
+                this.scenario, test => new TestFailed(test, default, "failed", exception), this.cancellationTokenSource);
     }
 }
